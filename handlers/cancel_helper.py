87abe38e8057fbe2e49e9df306098be93eeb5401
Fetrus Meilano Ilhamsyah <fetrusmeilano11@.com>
2026-03-28 06:06:20 +0700

    Fix ImportError and NameError in cancel_helper.py (Final Fix)

--- handlers/cancel_helper.py
+++ handlers/cancel_helper.py
@@ -6,19 +6,20 @@ from handlers.merge import _user_bg_tasks as merge_tasks, _user_locks as merge_l
 from handlers.vcftotxt import _user_bg_tasks as vcf2txt_tasks, _user_locks as vcf2txt_locks, _clear_buffers as vcf2txt_clear
 from handlers.txttovcf import _user_bg_tasks as ttv_tasks, _user_locks as ttv_locks, _clear_buffers as ttv_clear
 
-
 def cancel_all(user_id: int):
     """Batalkan semua proses aktif dan bersihkan memori/disk user."""
 
-    # Cancel semua timer debounce
-    for timers in [merge_timers, vcf2txt_timers, ttv_timers]:
-        timer = timers.pop(user_id, None)
-        if timer:
-            timer.cancel()
+    # Cancel semua background tasks
+    for tasks_dict in [merge_tasks, vcf2txt_tasks, ttv_tasks]:
+        tasks = tasks_dict.pop(user_id, set())
+        for t in tasks:
+            try: t.cancel()
+            except: pass
 
     # Hapus semua buffer disk
     for clear_func in [merge_clear, vcf2txt_clear, ttv_clear]:
-        clear_func(user_id)
+        try: clear_func(user_id)
+        except: pass
 
     # Hapus lock
     for locks in [merge_locks, vcf2txt_locks, ttv_locks]:
